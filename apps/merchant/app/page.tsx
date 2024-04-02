0b589c323dc1b73396b8ce3035b8797ff6e2a4bc
= <=>
2024-04-02 11:26:03 +0530

    bootstraping project

---
+++ apps/merchant/app/page.tsx
@@ -0,0 +1,10 @@
+
+
+
+export default function Page(): JSX.Element {
+  return (
+  <main>
+    <h2>merchant application</h2>
+  </main>
+  );
+}
---
+++ apps/user/app/page.tsx
@@ -0,0 +1,10 @@
+
+
+
+export default function Page(): JSX.Element {
+  return (
+  <main>
+    <h1 className="text-red-600">welocome to  cashmate</h1>
+  </main>
+  );
+}
